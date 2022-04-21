bda5e7f58e7950870a77272eb8ab2892a46a82e9
Rafhael <rafhael.pereira@gft.com>
2022-04-21 08:23:10 -0300

    prepared for build image

--- src/main/java/com/studies/brewery/inventoryfailover/config/RouterConfig.java
+++ src/main/java/com/studies/brewery/inventoryfailover/config/RouterConfig.java
@@ -1,5 +1,6 @@
-package com.studies.brewery.inventoryfailover.web;
+package com.studies.brewery.inventoryfailover.config;
 
+import com.studies.brewery.inventoryfailover.web.InventoryHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
